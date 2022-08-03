68aec08af1f3abfd076dcce679e69f4496adf40b
RyC2014 <rclingler1986@gmail.com>
2022-08-03 10:36:34 -0400

    created function to set color of text area, added hour array

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const currentDayEl = document.querySelector('#currentDay');
-const saveBtnEl = document.querySelectorAll('.saveBtn');
-const timeBlockEl = document.querySelectorAll('.time-block');
-const currentTime = moment().format('H');
+let currentDayEl = document.querySelector('#currentDay');
+let saveBtnEl = document.querySelectorAll('.saveBtn');
+let timeBlockEl = document.querySelectorAll('.time-block');
+let currentTime = moment().format('H');
 currentTime = parseInt(currentTime);
+
+
+// changes the hour time slot into current hour
 
 let hour9Am = document.querySelector('.hour9').textContent;
 hour9Am = parseInt(hour9Am[0]);
@@ -30,3 +33,30 @@ hour4Pm = parseInt(hour4Pm[0]) + 12;
 
 let hour5Pm = document.querySelector('.hour5').textContent;
 hour5Pm = parseInt(hour5Pm[0]) + 12;
+
+var hours = [hour9Am, hour10Am, hour11Am, hour12Pm, hour1Pm, hour2Pm, hour3Pm, hour4Pm, hour5Pm];
+var articleArr = [
+  { text9am: '.hour9' },
+  { text10am: '.hour10' },
+  { text11am: '.hour11' },
+  { text12Pm: '.hour12' },
+  { text1Pm: '.hour1' },
+  { text2Pm: '.hour2' },
+  { text3Pm: '.hour3' },
+  { text4Pm: 'hour4' },
+  { text5Pm: '.hour5' },
+];
+
+
+function setColor() {
+    for (var i = 0; i < hours.length; i++) {
+      if (currentTime < hours[i]) {
+        timeBlockEl[i].classList.add('future');
+      } else if (currentTime === hours[i]) {
+        timeBlockEl[i].classList.add('present');
+      } else {
+        timeBlockEl[i].classList.add('past');
+      }
+    }
+  }
+
